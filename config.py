911a20c00ae066cf028355f2645653627f028f57
teoet6 <teoet6@protonmail.com>
2021-06-09 13:12:13 +0300

    Working wm

--- config.py
+++
@@ -1,4 +0,0 @@
-name = "tueswm"
-border_width = 3
-border_color_active = 0x00800000
-border_color_normal = 0x00808080
--- test.sh
+++ test.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 Xephyr -ac -screen 1920x1080 -br -reset -terminate :1 &
-DISPLAY=:1
-(sleep 1 && xterm) &
-exec ./tueswm
+DISPLAY=:1 exec ./tueswm
